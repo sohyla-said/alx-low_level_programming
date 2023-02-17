b1c5cd51f38e3a7c4bcbf78bde2c384d3d22d5aa
sohyla-said <sohylasaid2004@gmail.com>
2023-02-17 12:25:11 +0000

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	char low;
+	char alpha;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+		putchar(alpha);
 	putchar('\n');
 
 	return (0);
-}	
+}
