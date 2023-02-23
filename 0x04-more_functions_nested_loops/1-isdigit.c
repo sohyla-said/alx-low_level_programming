1d0be78ae678107c9f47b1892bbe292b1ffde44b
sohyla-said <sohylasaid2004@gmail.com>
2023-02-23 10:41:53 +0000

    is it a digit?

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isdigit - Checks for a digit
+ * @c: An input integer
+ * Return: 1 if is a digit or 0 if is not
+ */
+int _isdigit(int c)
+{
+
+	if (isdigit(c))
+		return(1);
+	else
+		return(0);
+}
