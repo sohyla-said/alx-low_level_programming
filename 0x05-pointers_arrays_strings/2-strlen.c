3d125bb0fff838b4f1b0817c322ca68e59799efd
sohyla-said <sohylasaid2004@gmail.com>
2023-02-27 12:14:26 +0000

    lenght of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: An input
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
