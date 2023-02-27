9b3999475ee80ae3f4417a9e8bd5cb257af3f453
sohyla-said <sohylasaid2004@gmail.com>
2023-02-27 12:21:43 +0000

    puts a string to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by anew line to stdout
+ * @str: an input
+ * Return: Nothing
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
