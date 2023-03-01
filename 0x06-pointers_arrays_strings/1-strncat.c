9d111bd48c620ea10656cdb9b109afcc9cd5b50d
sohyla-said <sohylasaid2004@gmail.com>
2023-03-01 12:05:43 +0000

    strncat to concatenate 2 strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,6 +5,7 @@
  * _strncat - concatenates 2 strings
  * @dest: an input
  * @src: an input
+ * @n: an input
  * Return:a pointer to the resulting dtring
  */
 char *_strncat(char *dest, char *src, int n)
