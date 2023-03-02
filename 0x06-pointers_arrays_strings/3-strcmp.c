e245b1cd7fb86d691074b08b9d365b13e6b3ffde
sohyla-said <sohylasaid2004@gmail.com>
2023-03-02 12:23:18 +0000

    strcmp 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (strcmp(s1, s2) > 0)
-		return (15);
-	if (strcmp(s1, s2) < 0)
-		return (-15);
-	else 
-		return (0);
+	int result = strcmp(s1, s2);
+
+	return (result);
 }
