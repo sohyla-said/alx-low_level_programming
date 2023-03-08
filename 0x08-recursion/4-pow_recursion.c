fb61b5e5f611e004471fadabb49c33b230eae75d
sohyla-said <sohylasaid2004@gmail.com>
2023-03-08 12:29:00 +0000

    calculate power using recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x to the power of y
+ * @x: an input
+ * @y: an input
+ * Return: x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
