abd0236a1aa9321a825c23e93cce5b85de3894a9
sohyla-said <sohylasaid2004@gmail.com>
2023-03-13 12:14:23 +0000

    concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,15 +10,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *new;
+	char *new, *str1, *str2;
 	int i = 0, len1 = strlen(s1), len2 = strlen(s2);
+
+	str1 = s1;
+	str2 = s2;
 
 	if (s1 == NULL)
 		s1 = "";
+	s1 = str1;
+
 	if (s2 == NULL)
 		s2 = "";
+	s2 = str2;
 
 	new = malloc(sizeof(char) * (len1 + len2 + 1));
+	str1 = new;
 
 	if (new != NULL)
 	{
@@ -36,7 +43,7 @@ char *str_concat(char *s1, char *s2)
 			}
 		}
 		new[i] = '\0';
-		return (new);
+		return (str1);
 	}
 	else
 		return (NULL);
