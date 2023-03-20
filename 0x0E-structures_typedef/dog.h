814c10f18701091b9987491c1567ee415bdf42af
sohyla-said <sohylasaid2004@gmail.com>
2023-03-20 16:32:18 +0000

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - structure for a dog
+ * @name:dog's name
+ * @age: dog;s age
+ * @owner: dog's owner
+ * Description: data structure of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
