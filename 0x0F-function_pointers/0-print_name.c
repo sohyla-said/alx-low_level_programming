00d696fa2450d06fe6fcc7b83df26ef427516f48
sohyla-said <sohylasaid2004@gmail.com>
2023-03-22 12:27:11 +0000

    function to print a name using func ptr

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name
@@ -8,5 +9,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL || f == NULL)
+		return;
 	f(name);
 }
