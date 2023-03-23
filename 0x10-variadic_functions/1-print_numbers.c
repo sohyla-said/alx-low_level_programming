e5b2789bf9befbfa148770931cc8c0b98b00ad23
sohyla-said <sohylasaid2004@gmail.com>
2023-03-23 21:32:27 +0000

    print numbers(parameters) var func

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,11 +12,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	int num;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, int));
+		num = va_arg(ap, int);
+		printf("%d", num);
 		if (separator == NULL)
 			continue;
 		if (i < n - 1)
