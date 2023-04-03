a493c3d73411e92ca315712a671b3e1e7601987b
sohyla-said <sohylasaid2004@gmail.com>
2023-04-03 15:33:21 +0000

    free a list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,12 +10,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *ptr;
 
-	ptr = head;
 	while (head)
 	{
 		ptr = head;
 		head = head->next;
-		free(ptr->next);
 		free(ptr);
 	}
 	free(head);
